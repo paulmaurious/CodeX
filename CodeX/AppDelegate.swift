394c7684f79dd9c23dfcfab2ae8155ec6a16b365
paulmaurious <paulmaurious@gmail.com>
2016-08-04 17:55:31 +0530

    Initial Commit

---
+++ CodeX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CodeX
+//
+//  Created by Maurious Paul on 04/08/16.
+//  Copyright © 2016 Themelios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
